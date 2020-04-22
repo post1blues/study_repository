524c4b0310c91fe984ac999459ffcba31ed86049
post1blues <yura1kovalyov@gmail.com>
2020-04-22 18:03:27 +0300

    added phone_book.py and sorting.py

---
+++ phone_book.py
@@ -0,0 +1,116 @@
+
+
+class PhoneBook:
+    def __init__(self) -> None:
+        self.users = []
+
+    def show_users(self) -> list:
+        return self.users
+
+    def find_users_by_name(self, name: str) -> list:
+        found_users = []
+        for user in self.users:
+            for user_name in user.names:
+                if user_name.name.lower() == name.lower():
+                    found_users.append(user)
+                    break
+        return found_users
+
+    def find_users_by_surname(self, surname: str) -> list:
+        found_users = []
+        for user in self.users:
+            if user.surname.surname.lower() == surname.lower():
+                found_users.append(user)
+        return found_users
+
+    def find_users_by_phonenumber(self, phone_number: int) -> list:
+        found_users = []
+        for user in self.users:
+            for phone in user.phone_numbers:
+                if phone.phone_number == phone_number:
+                    found_users.append(user)
+                    break
+        return found_users
+
+    def create_user(self, names: list, surname: str, phone_numbers: list) -> object:
+
+        for phone_number in phone_numbers:
+            if self.find_users_by_phonenumber(phone_number):
+                return None
+
+        phone_numbers = [PhoneNumber(phone_number) for phone_number in phone_numbers]
+        surname = Surname(surname)
+        names = [UserName(name) for name in names]
+
+        new_user = User(names=names, surname=surname, phone_numbers=phone_numbers)
+        self.users.append(new_user)
+        return new_user
+
+
+class User:
+    def __init__(self, names: list, surname: object, phone_numbers: list) -> None:
+        self.names = names
+        self.surname = surname
+        self.phone_numbers = phone_numbers
+
+    def __repr__(self):
+        return f'Class User: {self.surname}, {self.names}, {self.phone_numbers}. '
+
+
+class UserName:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __repr__(self):
+        return f'UserName class {self.name}'
+
+
+class Surname:
+    def __init__(self, surname: str) -> None:
+        self.surname = surname
+
+    def __repr__(self):
+        return f'Surname class {self.surname}'
+
+
+class PhoneNumber:
+    def __init__(self, phone_number: int) -> None:
+        self.phone_number = phone_number
+
+    def __repr__(self):
+        return f'PhoneNumber class {self.phone_number}'
+
+
+if __name__ == '__main__':
+    phone_book = PhoneBook()
+    test_users = [
+        (['John'], 'Smith', [123454321]),
+        (['Elsie'], 'Gagne', [123456789]),
+        (['Rachel'], 'Galbreath', [987654321]),
+        (['Cynthia'], 'Boyd', [123456789]),
+        (['Jack'], 'Mitchell', [111111111, 222222222]),
+        (['Ramona', 'Marie'], 'Henry', [333333333, 444444444]),
+    ]
+
+    for test_user in test_users:
+        new_test_user = phone_book.create_user(names=test_user[0], surname=test_user[1], phone_numbers=test_user[2])
+        if new_test_user is None:
+            print(f'User with this phone number {test_user[2]} is already exist.')
+        else:
+            print(f'User {new_test_user.surname.surname} was successfully created.')
+
+    print('Users was added to PhoneBook')
+
+    print('Here are all users')
+
+    for user in phone_book.users:
+        print(user)
+
+    print('Let find a user with a name Ramona')
+    print(phone_book.find_users_by_name('ramona'))
+
+    print('Let find a user with a phone number 333333333')
+    print(phone_book.find_users_by_name('ramona'))
+
+    print('Let find a user with a surname Mitchell')
+    print(phone_book.find_users_by_surname('mitchell'))
---
+++ sorting.py
@@ -0,0 +1,194 @@
+from abc import ABC, abstractmethod
+from collections.abc import Iterable
+from random import randint
+from time import time
+
+
+class Validation:
+
+    def __init__(self, obj: list) -> None:
+        self.obj = obj
+
+    def validate_data(self) -> bool:
+        if not self.__is_iterable(self.obj):
+            print('ERROR: Object is not iterable.')
+            return False
+        if not self.__is_integers_in_array(self.obj):
+            print('ERROR: Object contains some not-integer elements.')
+            return False
+        print('Object is valid')
+        return True
+
+    def __is_iterable(self, array: list) -> bool:
+        return isinstance(array, Iterable)
+
+    def __is_integers_in_array(self, array: list) -> bool:
+        for element in array:
+            if not isinstance(element, int):
+                return False
+        return True
+
+
+class Sort(ABC):
+    def __init__(self, obj: list) -> None:
+        self.array = obj
+        super(Sort, self).__init__()
+
+    @abstractmethod
+    def get_sorted_array(self):
+        pass
+
+
+class QuickSort(Sort):
+    def get_sorted_array(self) -> list:
+        def _get_sorted_array(array, low_num, high_num):
+            if low_num < high_num:
+                split_index = self.__partition(self.array, low_num, high_num)
+                _get_sorted_array(array, low_num, split_index)
+                _get_sorted_array(array, split_index + 1, high_num)
+
+        _get_sorted_array(self.array, 0, len(self.array) - 1)
+
+        return self.array
+
+    def __partition(self, array, low_num, high_num) -> int:
+        pivot = array[(low_num + high_num) // 2]
+        i = low_num - 1
+        j = high_num + 1
+
+        while True:
+            i += 1
+            while array[i] < pivot:
+                i += 1
+            j -= 1
+            while array[j] > pivot:
+                j -= 1
+            if i >= j:
+                return j
+            array[i], array[j] = array[j], array[i]
+
+
+class MergeSort(Sort):
+    def get_sorted_array(self) -> list:
+        return self.__merge_sort(self.array)
+
+    def __merge_sort(self, array) -> list:
+        if len(array) <= 1:
+            return array
+        mediana = len(array) // 2
+        left_array = self.__merge_sort(array[:mediana])
+        right_array = self.__merge_sort(array[mediana:])
+        return self.__merge(left_array, right_array)
+
+    def __merge(self, left_array, right_array) -> list:
+        sorted_array = []
+        left_array_index = right_array_index = 0
+        left_array_length, right_array_length = len(left_array), len(right_array)
+
+        for i in range(left_array_length + right_array_length):
+            if left_array_index < left_array_length and right_array_index < right_array_length:
+                if left_array[left_array_index] <= right_array[right_array_index]:
+                    sorted_array.append(left_array[left_array_index])
+                    left_array_index += 1
+                else:
+                    sorted_array.append(right_array[right_array_index])
+                    right_array_index += 1
+
+            elif left_array_index == left_array_length:
+                sorted_array.append(right_array[right_array_index])
+                right_array_index += 1
+            elif right_array_index == right_array_length:
+                sorted_array.append(left_array[left_array_index])
+                left_array_index += 1
+        return sorted_array
+
+
+class BubbleSort(Sort):
+    def get_sorted_array(self) -> list:
+        need_to_swap = True
+        while need_to_swap:
+            need_to_swap = False
+            for i in range(len(self.array) - 1):
+                if self.array[i] > self.array[i + 1]:
+                    self.array[i], self.array[i + 1] = self.array[i + 1], self.array[i]
+                    need_to_swap = True
+        return self.array
+
+
+class InsertionSort(Sort):
+    def get_sorted_array(self) -> list:
+        for i in range(1, len(self.array)):
+            item_to_insert = self.array[i]
+            j = i - 1
+            while j >= 0 and self.array[j] > item_to_insert:
+                self.array[j + 1] = self.array[j]
+                j -= 1
+            self.array[j + 1] = item_to_insert
+        return self.array
+
+
+class DefaultPythonSort(Sort):
+    def get_sorted_array(self) -> list:
+        return sorted(self.array)
+
+
+class SortContext:
+    def __init__(self) -> None:
+        self.current_sort = None
+
+    def set_quick_sort(self) -> None:
+        self.current_sort = QuickSort
+
+    def set_bubble_sort(self) -> None:
+        self.current_sort = BubbleSort
+
+    def set_insertion_sort(self) -> None:
+        self.current_sort = InsertionSort
+
+    def set_merge_sort(self) -> None:
+        self.current_sort = MergeSort
+
+
+if __name__ == '__main__':
+
+    objects_array = []  # массив обьектов сортировки
+    result = {}  # словарь для результатов
+    length_of_test_array = 10000  # длина тестового массива
+
+    # добавляем в массив обьекты сортировки
+    objects_array.append(InsertionSort([randint(1, 100) for _ in range(length_of_test_array)]))
+    objects_array.append(BubbleSort([randint(1, 100) for _ in range(length_of_test_array)]))
+    objects_array.append(MergeSort([randint(1, 100) for _ in range(length_of_test_array)]))
+    objects_array.append(QuickSort([randint(1, 100) for _ in range(length_of_test_array)]))
+    objects_array.append(DefaultPythonSort([randint(1, 100) for _ in range(length_of_test_array)]))
+
+    # проходим по массивам и выполняем сортировку, замеряем время сортировки и добавляем результаты в словарь
+    for obj in objects_array:
+        print(f'Sorting by {type(obj)}...' )
+        t1 = time()
+        obj.get_sorted_array()
+        t2 = time()
+        delta_time = t2 - t1
+        result[f'{type(obj)}'] = delta_time
+
+    # делаем сортировку по скорости
+    result = {k: v for k, v in sorted(result.items(), key=lambda item: item[1])}
+
+    for element in result:
+        print(f'{element} ==> {result[element]}')
+
+    # test_array = [randint(1, 100) for _ in range(1000)]
+    # context = SortContext()
+    # if context.current_sort is None:
+    #     print('Current sort method is not set.')
+    #
+    # context.set_bubble_sort()
+    # bubble = context.current_sort(test_array)
+
+
+
+
+
+
+
+
